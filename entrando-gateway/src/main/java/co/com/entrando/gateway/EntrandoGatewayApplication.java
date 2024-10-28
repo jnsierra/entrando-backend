5279e236a2bb68aff9a46c6a150ab713e07379d6
Jesus Nicolas Sierra Chaparro <nicolas@iMac-de-Jesus.local>
2024-10-27 23:01:59 -0500

    feat(gateway): Configuracion inicial gateway

---
+++ entrando-gateway/src/main/java/co/com/entrando/gateway/EntrandoGatewayApplication.java
@@ -0,0 +1,13 @@
+package co.com.entrando.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrandoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrandoGatewayApplication.class, args);
+	}
+
+}
